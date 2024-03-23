1f423bcc5e51f5f64cf3fae5ef522d5bb78bf0d4
Iure Sales <iureguimaraes@gmail.com>
2024-03-23 00:35:05 -0400

    rgb

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,3 +1,12 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+LTO_ENABLE = yes
+RGB_MATRIX_DRIVER = WS2812
+RGBLIGHT_SUPPORTED = yes
+RGB_MATRIX_SUPPORTED = yes
