27db3b9dbae0c21f218e2eae8df04ecbb036152d
Iure <isales@beenverified.com>
2024-03-25 16:54:29 -0400

    Enable RGB underglow and update custom configuration

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,11 +2,10 @@
 # https://github.com/manna-harbour/miryoku
 
 BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 LTO_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
 RGBLIGHT_SUPPORTED = yes
-RGB_MATRIX_SUPPORTED = yes
