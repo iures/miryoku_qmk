2dd0a1d7e838add50ab9785a71a0b996dc5fc3e2
Iure <isales@beenverified.com>
2024-03-25 17:44:02 -0400

    odfserer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -8,7 +8,7 @@
 //#define PERMISSIVE_HOLD
 
 // #define TAPPING_TERM_PER_KEY
-#define TAPPING_TERM 150
+#define TAPPING_TERM 200
 #define TAPPING_TERM_THUMB 125
 
 // If you press a dual-role key, press another key, and then release the
